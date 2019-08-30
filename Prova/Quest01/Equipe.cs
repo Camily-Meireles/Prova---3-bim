7a27c9ac31b7a48602ddecc8e6e1e289ae6f7053
Camily Kirlly Pereira Meireles <kirlly.p@academico.ifrn.edu.br>
2019-08-30 12:23:09 -0300

    arquivo

---
+++ Prova/Quest01/Equipe.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest01
+{
+    class Equipe
+    {
+        private string nome;
+        public void SetNome( string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return nome;
+        }
+
+        private Jogador[] jogs = new Jogador[10];
+        int k;
+
+        public void Inserir(Jogador j)
+        {
+            if (k < 10)
+            {
+                jogs[k++] = j;
+            }
+        }
+        public Jogador[] Jogadores()
+        {
+            Jogador[] jog = new Jogador[k];
+            Array.Copy(jogs, jog, k); 
+            return jog;
+        }
+        public Jogador[] Lideres()
+        {
+            Jogador[] lider = new Jogador[k];
+            Array.Copy(jogs, lider, k);
+            return lider;
+        }
+
+
+    }
+}
---
+++ Prova/Quest01/Jogador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quest01
+{
+    class Jogador
+    {
+        private string nome, apelido;
+        private bool lider;
+        
+
+        public Jogador(string nome, string apelido, bool lider)
+        {
+            this.nome = nome;
+            this.apelido = apelido;
+            this.lider = lider;
+        }
+        
+        
+        public override string ToString() 
+        {
+
+            return $"{nome} -{apelido} {lider}";
+        }
+    }
+    
+}
